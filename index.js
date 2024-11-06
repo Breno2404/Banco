f68c306d05313e7dd310096d06c97928be57aa5d
charlen douglas <brenojosesilvasoares@gmail.com>
2024-11-06 15:29:12 -0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-console.log("teste")
+const email = "teste@teste.com"
+const senha = "123456"
+localStorage.setItem(email, senha)
+
+const emailInput = document.getElementById("email")
+const senhaInput = document.getElementById("senha")
+const formulario = document.getElementById("formulario_login")
+
+formulario.addEventListener("submit", (event) =>  {
+    event.preventDefault()
+    console.log(emailInput.value)
+    console.log(senhaInput.value)
+})
