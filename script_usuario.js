01e3daaf6a54e59ecef8ffb5d96abb2c7cd109fc
charlen douglas <brenojosesilvasoares@gmail.com>
2024-11-12 17:27:57 -0300

    Add files via upload

--- script_usuario.js
+++ script_usuario.js
@@ -1,16 +1,15 @@
 const usuarios = JSON.parse(localStorage.getItem("usuarios"))
+const tabela = document.getElementById("tabela-usuarios")
 
 for (let index = 0; index < usuarios.lenght; index++){
     const usuario = usuarios[index];
-    console.log(usuario.id, usuario.email, usuario.senha)
-
+   
     const linha = `
         <tr>
-            <th>id</th>
-            <td>email</td>
-            <td>senha</td>
+            <th>${usuario.id}</th>
+            <td>${usuario.email}</td>
+            <td>${usuario.senha}</td>
         </tr>
     `
-
-    console.log(linha)
+   tabela.innerHTML += linha
 }
